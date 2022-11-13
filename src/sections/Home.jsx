1e4b28e161b258a6ad825e5860f5462a2d1e4d9a
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-11-13 14:10:54 -0700

    Homepage complete

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,21 +1,29 @@
 import { useNav } from "../hooks/useNav"
 
+import styles from "../styles/Home.module.css";
+
 export default function Home() {
 
     const homeRef = useNav("Home");
 
+    const handleClickRentals = () =>{
+        document.getElementById("rentalsSection").scrollIntoView({behavior: "smooth"})
+    }
+
+    const handleClickContact = () =>{
+        document.getElementById("contactSection").scrollIntoView({behavior: "smooth"})
+    }
+
     return (
-        <section ref={homeRef} id="homeSection" className="secondary-section">
-            <h2>
-                Home
+        <section ref={homeRef} id="homeSection" className={styles.homeSection}>
+            <h2 className={styles.h2}>
+                Bodhi Property Management
             </h2>
-            <p>
-            Lorem ipsum dolor sit amet consectetur 
-            adipisicing elit. Commodi harum aut dolore,
-            officiis provident labore enim. Voluptatem 
-            vero repudiandae itaque tempore veritatis asperiores aliquam! 
-            Dolor natus molestias voluptatum tempore accusamus!
-            </p>
+            <h3 className={styles.h3}>Crested Butte, Colorado</h3>
+            <div className={styles.buttonContainer}>
+                <button className={styles.button} onClick={handleClickRentals}>RENTALS</button>
+                <button className={styles.button} onClick={handleClickContact}>CONTACT</button>
+            </div>
         </section>
     )
 }
