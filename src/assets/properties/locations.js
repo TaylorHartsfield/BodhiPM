f99b80c05480346da1df79ecf91402a45306a400
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-11-13 15:43:02 -0700

    start on rental neighborhood cards

---
+++ src/assets/properties/locations.js
@@ -0,0 +1,7 @@
+export const locations = [
+    {key:"mount", title: "Mount Crested Butte", blurb: "Ski, bike, and play just steps outside of your front door." },
+    {key:"town", title: "Crested Butte", blurb: "Centrally located in Crested Butte's, historic distric,\
+    walk to dinner, live music, and shopping. Hop on the free local shuttle to the mountian for a day of skiing or hiking." },
+    {key: "south", title: "Crested Butte South", blurb: "Located just 10 minutes south of town, escape the\
+    hustle and bustle for true vacation serenity."  },
+]
--- src/sections/Rentals.jsx
+++ src/sections/Rentals.jsx
@@ -1,21 +1,58 @@
 import { useNav } from "../hooks/useNav";
+import styles from "../styles/Locations.module.css";
+import {locations} from "../assets/properties/locations.js";
+import {Row} from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import town from "../assets/properties/img/town.webp";
+
 
 export default function Rentals() {
+
 
     const rentalsRef = useNav("Rentals");
 
     return (
-        <section  ref={rentalsRef} id="rentalsSection">
-            <h2>
-                Rentals
-            </h2>
-            <p>
-            Lorem ipsum dolor sit amet consectetur 
-            adipisicing elit. Commodi harum aut dolore,
-            officiis provident labore enim. Voluptatem 
-            vero repudiandae itaque tempore veritatis asperiores aliquam! 
-            Dolor natus molestias voluptatum tempore accusamus!
-            </p>
+        <section ref={rentalsRef} id="rentalsSection" className={styles.rentalsContainer}>
+            <h2 className={styles.h2}>Rentals</h2>
+            <h3 className={styles.h3}>Neighborhoods</h3>
+            <div className={styles.container}>
+                <Row className={styles.row}>
+                <Card className={styles.card} key={locations[0].title}>
+                        <Card.Body className={locations[0].key}>
+                            <Card.Title>
+                                {locations[0].title}
+                            </Card.Title>
+                            <Card.Text>
+                                {locations[0].blurb}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+
+                    <Card className={styles.card} key={locations[1].title}>
+                        <Card.Body className={locations[1].key}>
+                            <Card.Title>
+                                {locations[1].title}
+                            </Card.Title>
+                            <Card.Text>
+                                {locations[1].blurb}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+        
+                <Card className={styles.card} key={locations[2].title}>
+                        <Card.Body>
+                            <Card.Title>
+                                {locations[2].title}
+                            </Card.Title>
+                            <Card.Text>
+                                {locations[2].blurb}
+                            </Card.Text>
+                        </Card.Body>
+                 </Card>
+                 </Row>
+            </div>
+        
+
         </section>
     )
 }
