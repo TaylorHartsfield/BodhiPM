d15293b7b82cbc73616bbc793f8687e9bcd865ed
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-11-13 18:48:43 -0700

    collapse navbar for responsive behavior

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,16 @@ import styles from "../styles/Header.module.css";
 import logo from "../assets/img/BodhiPMTrans.png";
 import { useContext } from "react";
 import { NavContext } from "../context/NavContext";
+import { useState } from "react";
+
 
 export default function Header() {
+
+    const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+    function handleClick() {
+        setIsNavExpanded(!isNavExpanded)
+    }
 
     const { activeLinkId } = useContext(NavContext);
     
@@ -25,15 +33,15 @@ export default function Header() {
 
 
         return(
-            <ul key={content}>
-                <li>
+            
+                <li key={content}>
                     <button
                     onClick={handleClickNav}
                     className={activeLinkId === content ? styles.activeClass : ""}>
                     {content}
                     </button>
                 </li>
-            </ul>
+
         )
     }
 
@@ -41,16 +49,35 @@ export default function Header() {
     return (
         <header className={styles.header}>
             <div className={styles.container}>
-                <div className={styles.navContainer}>
-                    <img className={styles.logo} src={logo} alt="logo"
-                    onClick={handleClickLogo}/>
-                    <nav>
-                        {navLinks.map(nav => renderNavLink(nav))}
-                    </nav>
-
-                </div>
+                    <div className={styles.brandName}>
+                        <img 
+                        className={styles.logo} 
+                        src={logo} 
+                        alt="logo"
+                        onClick={handleClickLogo}/>
+                    </div>
+                    <button className={styles.hamburger}
+                        onClick={handleClick}>
+                            <svg xmlns="http://www.w3.org/2000/svg" 
+                            fill="none" 
+                            viewBox="0 0 24 24" 
+                            strokeWidth={1.5} stroke="currentColor" 
+                            className="w-6 h-6">
+                            <path strokeLinecap="round" 
+                            strokeLinejoin="round" 
+                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                            </svg>
+                        </button>
+                    <div
+                        className={
+                        isNavExpanded ? 
+                        styles.navigationMenuExpanded : 
+                        styles.navigationMenu}>
+                        <ul>
+                            {navLinks.map(nav => renderNavLink(nav))}
+                        </ul>
+                    </div>    
             </div>
-
         </header>
     )
 }
