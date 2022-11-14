175b2f0d01d7518e745b452b53d63fbcca314eca
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-11-14 10:59:04 -0700

    bio updated

--- src/sections/AboutUs.jsx
+++ src/sections/AboutUs.jsx
@@ -23,17 +23,15 @@ export default function AboutUs() {
                     </Col>
                     <Col style={{justifyContent:"center", alignItems:"center"}}>
                     <p style={{textAlign: "left", paddingTop: "1rem"}}>
-                     Hi there! <br/> I'm Carolyn Whitlow (right) - 
-                     founder and CEO of Bodhi Property Management. This is my wife Taylor and our
-                     Saint Bernard, Bodhi. I have lived in the valley since 2016 and love everything
-                     that this beautiful valley holds. Bodhi PM was founded in 2020 from a dream to help others
-                     enjoy the magic that Crested Butte has to share. Bodhi PM specializes in all 
-                     short-term rental services such as rate management, property care,
-                     guest and vendor communication, conceierge services and more. We also customize packages for 
-                     long-term care of non-rental second homes.
-                     Whether you are looking for someone to help you manage your home,
-                     or looking to book your next vacation, Bodhi Property Management can help! 
-                     I look forward to helping you and your family enjoy Crested Butte just as much as we do!
+                    Hi there!<br/> I'm Carolyn Whitlow (right) - founder and CEO of Bodhi Property Management. 
+                    This is my wife Taylor and our Saint Bernard, Bodhi. I have lived in the valley since 2016 and love 
+                    everything that this beautiful valley holds. Bodhi PM was founded in 2020 from a dream to help others enjoy the 
+                    magic that Crested Butte has to share. Bodhi PM is a fully woman owned and operated property management company 
+                    located in Crested Butte, Colorado. We specialize in all short-term rental services such as rate management, 
+                    property care, guest and vendor communication, concierge services and more. We also customize packages for 
+                    long-term care of non-rental second homes. Whether you are looking for someone to help you manage your home, or 
+                    looking to book your next vacation, Bodhi Property Management can help! I look forward to helping you 
+                    and your family create memories in Crested Butte that will last a lifetime.
                      </p>
                     </Col>
                 </Row>
