823cc79a3394a531e26ef3866769c90c22bfbaf0
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-11-14 15:10:55 -0700

    Contact page styling complete

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -2,49 +2,80 @@ import { useNav } from "../hooks/useNav";
 import {Col, Form, Button} from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import styles from "../styles/Contact.module.css";
+import { useState } from "react";
 
 export default function Contact() {
 
     const contactRef = useNav("Contact");
 
-    function handleOnClick() {
-       console.log("clicked")
-    }
+    const [focusName, setFocusName] = useState(false);
+    const [focusEmail, setFocusEmail] = useState(false);
+    const [focusMessage, setFocusMessage] = useState(false);
+
+    function handleFocus(e) {
+       if(e.target.id === "name") {
+        setFocusName(true)
+       } else if (e.target.id === "email") {
+        setFocusEmail(true)
+       } else {
+        setFocusMessage(true)
+       }
+       }
+    
+       function handleBlur(e) {
+        if(e.target.id === "name" && e.target.value === "") {
+            setFocusName(false)
+           } else if (e.target.id === "email" && e.target.value === "") {
+            setFocusEmail(false)
+           } else if (e.target.value === "") {
+            setFocusMessage(false)
+           }
+           }
+       
 
     return (
         <section ref={contactRef} id="contactSection" className="contactSection">
             <h2>
-                Contact
+                Contact Us
             </h2>
             <Row>
-                <Col><p>
-            If you have a booking inquiry or would like to connect about services for your property, please fill out the form below.<br/>
-            I will be in touch with you as soon as possible!
-            </p>
-            </Col>
-            <Col>
+                <Col></Col>
+                <Col lg={6} sm={9}>
+                <p style={{textAlign:"center"}}>
+                For additional information regarding vacation bookings or property services, please fill out the form below with as much information as possible including property 
+                location, services needed, and any other applicable requests you may have.
+                <br/>
+                A member of the Bodhi Property Management team will be in touch with you as soon as possible.<br/> Thanks for stopping by!
+                </p>
+                </Col>
+                <Col></Col>
+           <Row>
+            <Col></Col>
+            <Col lg={6} sm={9}>
                 <Form>
                     <Form.Group className={styles.formGroup}>
-                        <Form.Label for="name">
+                        <Form.Label for="name" className={focusName? styles.notEmpty : null}>
                             NAME
                         </Form.Label>
-                        <Form.Control onClick={handleOnClick} type="text" required className={styles.formControl}/>
+                        <Form.Control onFocus={handleFocus} onBlur={handleBlur} type="text" id="name" required className={styles.formControl}/>
                     </Form.Group>
                     <Form.Group  className={styles.formGroup}>
-                        <Form.Label for="email">
+                        <Form.Label for="email" className={focusEmail? styles.notEmpty : null}>
                             EMAIL
                         </Form.Label>
-                        <Form.Control onClick={handleOnClick} type="email" required className={styles.formControl}/>
+                        <Form.Control onFocus={handleFocus} onBlur={handleBlur} type="email" id="email" required className={styles.formControl}/>
                     </Form.Group>
                     <Form.Group  className={styles.formGroup}>
-                        <Form.Label for="message">
+                        <Form.Label for="message"  className={focusMessage? styles.notEmpty : null}>
                             Message
                         </Form.Label>
-                        <Form.Control onClick={handleOnClick} type="textarea" required className={styles.formControl}/>
+                        <Form.Control style={{marginBottom:"1rem"}}onFocus={handleFocus} onBlur={handleBlur} as="textarea" id="message" rows="4" required className={styles.formControl}/>
                     </Form.Group>
-                    <Button type="submit">Submit</Button>
+                    <Button className={styles.button} type="submit">Submit</Button>
                 </Form>
             </Col>
+            <Col></Col>
+            </Row>
             </Row>
         </section>
     )
