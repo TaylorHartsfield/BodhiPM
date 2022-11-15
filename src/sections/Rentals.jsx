31ca658ff92efa0d9f5e3a95f3ad1fea9e491ac7
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-11-14 17:20:44 -0700

    Neighborhood Cards initial commit

--- src/sections/Rentals.jsx
+++ src/sections/Rentals.jsx
@@ -1,9 +1,12 @@
 import { useNav } from "../hooks/useNav";
 import styles from "../styles/Locations.module.css";
 import {locations} from "../assets/properties/locations.js";
-import {Row} from "react-bootstrap";
-import { Card } from "react-bootstrap";
-
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import town from "../assets/properties/img/TownCB.jpeg";
+import mount from "../assets/properties/img/Mount.jpeg";
+import south from "../assets/properties/img/South.jpeg";
 
 export default function Rentals() {
 
@@ -16,42 +19,53 @@ export default function Rentals() {
             <h3 className={styles.h3}>Neighborhoods</h3>
             <div className={styles.container}>
                 <Row className={styles.row}>
-                <Card className={styles.card} key={locations[0].title}>
-                        <Card.Body className={locations[0].key}>
+                    <Col className={styles.col} xs={10} sm={9} md={6} lg={4}>
+                        <Card className={styles.town} key={locations[1].title}>
+                        <div className={styles.cardImgBlock}>
+                            <Card.Img src={town} alt="Town" className={styles.img} />
+                            </div>
+                            <Card.Body className={styles.body}>
+                                <Card.Title>
+                                <h5 className={styles.h5}>{locations[1].title}</h5>
+                                </Card.Title>
+                                <Card.Text>
+                                <p className={styles.p}>{locations[1].blurb}</p>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className={styles.col} xs={10} sm={9} md={6} lg={4}>
+                        <Card className={styles.mt} key={locations[0].title}>
+                            <div className={styles.cardImgBlock}>
+                                <Card.Img src={mount} alt="Mt.CB" className={styles.img}/>
+                            </div>
+                            <Card.Body className={styles.body}>
+                                <Card.Title>
+                                    <h5 className={styles.h5}>{locations[0].title}</h5>
+                                </Card.Title>
+                                <Card.Text>
+                                    <p className={styles.p}>{locations[0].blurb}</p>
+                                </Card.Text>
+                             </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className={styles.col} xs={10} sm={9} md={6} lg={4}>
+              <Card className={styles.south} key={locations[2].title}>
+                <div className={styles.cardImgBlock}>
+                 <Card.Img src={south} alt="CB South" className={styles.img} />
+                 </div>
+                 <Card.Body className={styles.body}>
                             <Card.Title>
-                                {locations[0].title}
+                                <h5 className={styles.h5}>{locations[2].title}</h5>
                             </Card.Title>
                             <Card.Text>
-                                {locations[0].blurb}
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-
-                    <Card className={styles.town} key={locations[1].title}>
-                        <Card.Body className={locations[1].key}>
-                            <Card.Title>
-                                {locations[1].title}
-                            </Card.Title>
-                            <Card.Text>
-                                {locations[1].blurb}
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-        
-                <Card className={styles.card} key={locations[2].title}>
-                        <Card.Body>
-                            <Card.Title>
-                                {locations[2].title}
-                            </Card.Title>
-                            <Card.Text>
-                                {locations[2].blurb}
+                            <p className={styles.p}>{locations[2].blurb}</p>
                             </Card.Text>
                         </Card.Body>
                  </Card>
+                 </Col>
                  </Row>
             </div>
-        
-
         </section>
     )
 }
